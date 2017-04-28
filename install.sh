6feb07733c40831a6c3a2ab4b83787605af31cfe
Julien Stroheker <julienstroheker@gmail.com>
2017-04-28 13:30:56 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,4 @@ ROLE=$2
 mkdir /tmp/dcos
 cd /tmp/dcos
 curl -O http://${BOOTSTRAP_URL}/dcos_install.sh
-curl -X POST --data-urlencode 'payload={"channel": "#dh-paas-infra-azure", "username": "DeploymentBot", "text": "Deploying '${ROLE}' on '$HOSTNAME'"}' https://hooks.slack.com/services/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
 sudo bash dcos_install.sh ${ROLE}
-curl -X POST --data-urlencode 'payload={"channel": "#dh-paas-infra-azure", "username": "DeploymentBot", "text": "Deployment '${ROLE}' done on '$HOSTNAME'"}' https://hooks.slack.com/services/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
